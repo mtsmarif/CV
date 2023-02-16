d5e608b0c392c3bec87a9f47308c554d93be8f27
mtsmarif <arifmtsm@gmail.com>
2023-02-16 16:20:18 +0700

    first commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,35 @@
+// work data
+
+export const Work = [
+    
+    {
+        id:1,
+        name:"PT Dana indonesia",
+        description:"Memelihara, memantau kinerja server, troubleshoot problem server, mounting, plugin dan plugout hardware server.",
+        tags:["Enginer Data Center","ITSUPPORT"],
+        
+        demo:"https://www.linkedin.com/in/arif-mutashim-62847519a/",
+        github:"https://github.com/mtsmarif"
+    },
+   
+    {
+        id:1,
+        name:"PT KEB Hana Bank Indonesia",
+        description:"Bertanggung jawab atas divisi layanan terkelola, Memperbaiki bugs dan menyelesaikan case terkait jaringan dan komputer error by ticket .",
+        tags:["Enginer Data Center","ITSUPPORT"],
+        
+        demo:"https://www.linkedin.com/in/arif-mutashim-62847519a/",
+        github:"https://github.com/mtsmarif"
+        
+    },
+    {
+        id:1,
+        name:"PT Traveloka",
+        description:"Menjadi Team Leader, dan mengontrol kinerja by aplikasi dalam melaksanakan marketing lapangan di bandara Soekarno Hatta,", 
+        tags:["Digital Marketing"],
+        
+        demo:"https://www.linkedin.com/in/arif-mutashim-62847519a/",
+        github:"https://github.com/mtsmarif"
+        
+    },
+] 
